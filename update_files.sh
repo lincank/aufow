67eab82a74ca88655b2c35e22e5683cc00313a2e
lincank <lincank@gmail.com>
2013-11-20 00:24:10 +0800

    bug fix

--- update_files.sh
+++ update_files.sh
@@ -5,12 +5,14 @@
 # cron job:
 # 00 11 * * * root /etc/openvpn/update_files.sh
 
-DIR=/etc/openvpn/
+DIR='/etc/openvpn/'
+DOMAIN='http://www.everants.com/'
+FILES=('custom_routes' 'gfwdomains.conf' 'except_routes' 'gfw_routes');
 
-FILES=(custom_routes gfwdomains.conf except_routes gfw_routes)
-
-for file in FILES
+for file in ${FILES[@]}
 do
 	echo "Updating file: ${file}"
-	wget -P ${DIR} -N ${DIR}${file}	
+	cmd="wget -P ${DIR} -S ${DOMAIN}${file}"
+	echo "Executing: ${cmd}"
+	`${cmd}`
 done
