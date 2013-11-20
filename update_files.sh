ed57b454df39cb9542048131295d56ff75da29e1
lincank <lincank@gmail.com>
2013-11-20 15:36:08 +0800

    update routes fetching script

--- update_files.sh
+++ update_files.sh
@@ -3,16 +3,24 @@
 #
 # Update routes reloated files from server
 # cron job:
-# 00 11 * * * root /etc/openvpn/update_files.sh
+# 00 11 * * * root /etc/openvpn/update_files.sh 2>&1 >> /tmp/update_files.log
+# 
+# Works for ash shell, OpenWrt built-in
 
 DIR='/etc/openvpn/'
 DOMAIN='http://www.everants.com/'
-FILES=('custom_routes' 'gfwdomains.conf' 'except_routes' 'gfw_routes');
 
-for file in ${FILES[@]}
+# ash style array in this way
+FILES="custom_routes gfwdomains.conf except_routes gfw_routes";
+
+echo "*********** routes update script starts ***********"
+echo "[`date`]"
+for file in $FILES
 do
-	echo "Updating file: ${file}"
-	cmd="wget -P ${DIR} -S ${DOMAIN}${file}"
+	echo "---> Updating file: ${file} "
+	cmd="wget -P ${DIR} -N ${DOMAIN}${file}"
 	echo "Executing: ${cmd}"
 	`${cmd}`
 done
+
+echo "*********** routes update script ends ***********"
